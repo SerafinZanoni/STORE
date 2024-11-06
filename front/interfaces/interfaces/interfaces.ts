ee2f55d4a4f224cf36c21517c45ab6e6049aff4d
Serafin Zanoni <serazanoni@gmail.com>
2024-11-06 12:24:23 -0300

    carpeta front e interfaces

---
+++ front/interfaces/interfaces/interfaces.ts
@@ -0,0 +1,35 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+  role: string;
+}
+
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: string;
+  categoryId: number;
+}
+
+export interface ICategory {
+  id: number;
+  name: string;
+  products: IProduct[];
+}
+
+export interface Order {
+  id: number;
+  status: string;
+  date: Date;
+}
+
+export interface Credential {
+  id: number;
+  password: string;
+}
